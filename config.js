27ed838befa23a7b55bc5c083ff6993c28d05170
Vitus <vithush2809@gmail.com>
2020-11-10 11:07:43 +0100

    config lavet

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    databaseURI: 'mongodb+srv://BrownieNation:280991vt@cluster0.g8wqg.mongodb.net/ToastNu?retryWrites=true&w=majority',
+
+    localPort: 8080
+};
+
+module.exports = config;
