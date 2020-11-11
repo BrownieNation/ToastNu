f0539d8461ce01ceefb06c36ab108c1eb4937b4d
Magnus Heinesen <magnusoh96@gmail.com>
2020-11-11 12:12:18 +0100

    runnable app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const config = require('./config');
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+
+//app.use(require('./Routes/joke'));
+//app.use(require('./Routes/othersites'));
+//app.use(require('./Routes/otherjokes'));
+
+const port = process.env.PORT || config.localPort; // Heroku
+app.listen(port);
+console.log('Listening on port ' + port + ' ...');
+
+module.exports = app; // test
+
+
---
+++ model/user.js
@@ -0,0 +1,11 @@
+//user.js
+
+const mongoose = require('mongoose');
+
+const user = new mongoose.Schema({
+    name: String,
+    password: String,
+
+})
+
+module.exports = mongoose.model('User', User);
