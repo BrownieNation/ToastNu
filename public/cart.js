ec6cfe795370fada5af3e6d6bc8d7d8bd04dd4b9
Morten Degn-Pedersen <201500190@post.au.dk>
2020-11-22 21:16:34 +0100

    mangler checkud og amounttælleren

--- public/cart.js
+++ public/cart.js
@@ -1,4 +1,5 @@
 
+
 let products = [
     {
         name:'placeholder1',
@@ -65,9 +66,63 @@ function cartNumbers() {
 
 }
 
+function generatecartHTML(price,title,imgsrc)
+{
+    let theprice=parseFloat(price);
+    let toreturn =` 
+    <td>
+        <div class="row">
+            <div class="col-lg-2 Product-img">
+                <img id = "productImage"src="${imgsrc}" alt="..." class="img-responsive"/>
+            </div>
+            <div class="col-lg-10">
+                <h4 class="nomargin" id = "productName">${title}</h4>
+            </div>
+        </div>
+    </td>
+    <td id = "productPrice"> ${theprice} ,- </td>
+    <td data-th="Quantity">
+        <input id= "productAmount" type="number" class="form-control text-center" value="1">
+    </td>
+    <td class = "subtotal">${theprice} ,-</td>
+    <td class="actions" data-th="" style="width:10%;">
+        <button id="buttonRefresh" class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
+        <button id="buttonDelete" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
+    </td>
+    `;
+    return toreturn;
+}
+function addItemAmount(price,subtotal)
+{
+    let valofSub=parseFloat(subtotal.innerHTML.split(" ")[0]);
+    
+    
+}
+function removefromStorage(price,titel,imgsrc)
+{
+    let cartItemString=localStorage.getItem('cartitems');
+    let cartNumbers=localStorage.getItem('cartNumbers');
+    
+    let value= cartItemString.replaceAll(price+" ,-splithere"+titel+"splithere"+imgsrc+"__","");
+    //  console.log(price+" ,-splithere"+titel+"splithere"+imgsrc+"__","");
+    // cartNumbers=parseInt(cartNumbers);
+   
+    localStorage.setItem('cartitems',value);
+}
+function calculateTotal()
+{
+    
+    let totalprice=0;
+    let subtotals=document.getElementsByClassName('subtotal');
+    for(subtotal of subtotals)
+    {
+        totalprice+=parseFloat(subtotal.innerHTML.split(" ")[0]);
+    }
+    document.getElementById("totalAmount").textContent = "Total: " + (totalprice>0?totalprice + " ,-":"");
+}
 function generatecartItems()
 {
-    let cart= document.getElementsByClassName('table table-hover border bg-white')[0];
+    let cart= document.getElementsByClassName('cartbody')[0];
     let buyString=localStorage.getItem('cartitems');
     let arr=buyString.split("__");
     let finalstring=[];
@@ -83,64 +138,42 @@ function generatecartItems()
         tmp[0]=(tmp[0].replace(" ,-",""));
         finalstring.push(tmp);
     }
-    console.log(finalstring);
+    
 
     for(let i=0;i<finalstring.length-1;i++)
     {
-        let newitem= document.createElement('div');
-        newitem.innerHTML=` <tr>
-<td>
-    <div class="row">
-        <div class="col-lg-2 Product-img">
-            <img id = "productImage"src="${finalstring[i][2]}" alt="..." class="img-responsive"/>
-        </div>
-        <div class="col-lg-10">
-            <h4 class="nomargin" id = "productName">${finalstring[i][1]}</h4>
-            <p id = "productDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-    </div>
-</td>
-<td id = "productPrice"> ${parseFloat(finalstring[i][0])} ,- </td>
-<td data-th="Quantity">
-    <input id= "productAmount" type="number" class="form-control text-center" value="1">
-</td>
-<td id= "subtotal">12,000</td>
-<td class="actions" data-th="" style="width:10%;">
-    <button id="buttonRefresh" class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
-    <button id="buttonDelete" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
-</td>
-</tr>`;
-cart.appendChild(newitem);
-    }
-}
-// console.log(localStorage.getItem('cartitems'));
-generatecartItems();
+        let newitem=document.getElementById(finalstring[i][1]);
+        if(newitem==null)
+        {
+            newitem= document.createElement('tr');
+            newitem.id=finalstring[i][1];
+            newitem.className='items';
+            newitem.innerHTML=generatecartHTML(finalstring[i][0],finalstring[i][1],finalstring[i][2]);
+            cart.appendChild(newitem);
+            // newitem.getElementsByClassName('form-control text-center')[0].addEventListener('change',addItemAmount(parseFloat(finalstring[i][0]),newitem.getElementsByClassName('subtotal')[0]))
+            newitem.getElementsByClassName('btn btn-danger btn-sm')[0].addEventListener('click',function(event){
+                let target=event.target.parentElement.parentElement;
+                target.remove();
+                removefromStorage(finalstring[i][0],finalstring[i][1],finalstring[i][2]);
+                calculateTotal();
+            });
+        }
+        else
+        {
+            // tæller value op på  item i cart
+            let productAmount= newitem.getElementsByClassName('form-control text-center')[0];
+            productAmount.value=(parseInt(productAmount.value)+1);
+            let subtotal=newitem.getElementsByClassName('subtotal')[0];
+            let valofSub=parseFloat(subtotal.innerHTML.split(" ")[0]);
+            subtotal.innerHTML=parseFloat(finalstring[i][0])*parseInt(productAmount.value) + " ,-";
+            
+        }
 
-// let cart= document.getElementsByClassName('cartItems')[0];
-// console.log(cart.className);
-// let newitem= document.createElement('div');
-// newitem.innerHTML=` <tr>
-// <td>
-//     <div class="row">
-//         <div class="col-lg-2 Product-img">
-//             <img id = "productImage"src="${img}" alt="..." class="img-responsive"/>
-//         </div>
-//         <div class="col-lg-10">
-//             <h4 class="nomargin" id = "productName">${title}</h4>
-//             <p id = "productDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-//             tempor incididunt ut labore et dolore magna aliqua.</p>
-//         </div>
-//     </div>
-// </td>
-// <td id = "productPrice"> ${price} ,- </td>
-// <td data-th="Quantity">
-//     <input id= "productAmount" type="number" class="form-control text-center" value="1">
-// </td>
-// <td id= "subtotal">12,000</td>
-// <td class="actions" data-th="" style="width:10%;">
-//     <button id="buttonRefresh" class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
-//     <button id="buttonDelete" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
-// </td>
-// </tr>`;
-// cart.appendChild(newitem);
+        
+    }
+    calculateTotal();
+   
+    
+}
+
+generatecartItems();
