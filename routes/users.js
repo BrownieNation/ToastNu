dc350c5e0d15a1ec0c3eb2f3651a1dccf8855c7d
Morten Degn-Pedersen <201500190@post.au.dk>
2020-11-25 16:03:10 +0100

    Error

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,5 @@
 // toastnu.js
 const express = require('express');
-<<<<<<< HEAD
-// const app = express();
-=======
->>>>>>> 983231a6cf755570e9e551f38872b7e6c10f08ac
 const router = express.Router();
 const controller = require("../controller/controller");
 const User = require("../model/userSchema");
@@ -34,11 +30,6 @@ const User = require("../model/userSchema");
     //users
     router.get('/users', async (request, response) => {
         try {
-<<<<<<< HEAD
-            
-=======
-            console.log(" er i Users");
->>>>>>> 983231a6cf755570e9e551f38872b7e6c10f08ac
             const users = await controller.getUsers();
             if (!users) {
                 return response.status(404).send()
